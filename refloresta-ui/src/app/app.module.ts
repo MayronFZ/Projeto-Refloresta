87c6cf640989156bc67a162b310e49801a04a13c
unknown <mayronmartinscosta@hotmail.com>
2022-07-13 22:38:30 -0300

    Adicionado estrutura e css dos cards home

--- refloresta-ui/src/app/app.module.ts
+++ refloresta-ui/src/app/app.module.ts
@@ -1,17 +1,23 @@
-
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SobreComponent } from './components/pages/sobre/sobre.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { SobreComponent } from './components/pages/sobre/sobre.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SobreComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, MatTabsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
